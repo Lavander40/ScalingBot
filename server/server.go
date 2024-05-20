4c14bae688d35a561a532040fa3a6bd8ca08e152
Lavander40 <a.kirill.03@mail.ru>
2024-05-21 02:50:23 +0300

    init commit

--- server/server.go
+++ server/server.go
@@ -63,7 +63,6 @@ func (s *Server) HandleRequest(req []byte) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(userList)
 
 	for _, user := range userList {
 		msg := fmt.Sprintf("Внимание облако %s перегружено, значение предела %s%% преодолено, последнее значение метрики: %f", jsonReq.Title, jsonReq.Message, jsonReq.Alerts[0].Values.A)
